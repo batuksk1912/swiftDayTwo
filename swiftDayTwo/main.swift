5094d312ff5de75d3fb0215d2de1999b604ee2e5
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-02-08 18:11:38 -0500

    Initial Commit

---
+++ swiftDayTwo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftDayTwo
+//
+//  Created by Baturay Kayatürk on 8.02.2019.
+//  Copyright © 2019 LambtonCollege. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
