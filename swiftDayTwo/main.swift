486f2c64c684ffdb2c1146ed05ed17f881c8fb36
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-02-08 19:37:29 -0500

    Arrays.

--- swiftDayTwo/main.swift
+++ swiftDayTwo/main.swift
@@ -48,3 +48,51 @@ if (y.contains(-1000)) {
     print(true)
 }
 
+var abba = [10,20,30]
+
+for i in abba {
+    print(i)
+}
+
+print(abba[0])
+
+print(abba.capacity)
+
+//abba[5] = 500 out of range..
+
+var babba = [Int]()
+
+//babba[0] = 100
+
+print(babba)
+
+babba.append(150)
+
+print(babba[0])
+
+let c = abba + babba
+
+for k in c {
+    print(k)
+}
+
+for i in c[1...3] {
+    print(i, terminator: " * ")
+}
+
+var t = c[2...3]
+
+print(t)
+
+//print(t[0]) // its not working because not refer previous array.
+
+var z = Array<Int>()
+
+z.append(5000)
+
+print(z.capacity)
+
+print(z[0])
+
+
+
